3afa35162fca463e921f15150f7b45ff34844b7d
Erick Perez <erickjpl@gmail.com>
2023-09-23 18:03:19 -0400

    quitando las banderas

--- src/api/category/services/category.ts
+++ src/api/category/services/category.ts
@@ -7,9 +7,7 @@ import { factories } from '@strapi/strapi';
 export default factories.createCoreService('api::category.category', {
   async updateProducts (category: number) {
     const count = await strapi.db.query("api::product.product").count({ where: { category: category } })
-    console.info({ updateProducts: category, count })
 
     const update = await strapi.entityService.update('api::category.category', category, { data: { products: count } })
-    console.info({ update })
   }
 });
